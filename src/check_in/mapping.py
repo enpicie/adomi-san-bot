a4066ba9c2bcfe4c8ce3652e58552c89391058fb
NOAH LEE <iamnoah@myself.com>
2025-09-12 23:10:00 -0500

    fix command name

--- src/check_in/mapping.py
+++ src/check_in/mapping.py
@@ -2,7 +2,7 @@ from commands.types import CommandMapping
 import check_in.check_in as check_in
 
 checkin_commands: CommandMapping = {
-    "checkin": {
+    "check-in": {
         "function": check_in.check_in_user,
         "description": "Check in the calling user.",
         "params": []
