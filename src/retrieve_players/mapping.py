126c5c16afac70a971d733696d4f9c6b3f26bb62
Mega <lscomp509@gmail.com>
2025-09-16 19:14:18 -0400

    Changed imporrt in mapping

--- src/retrieve_players/mapping.py
+++ src/retrieve_players/mapping.py
@@ -1,13 +1,11 @@
-from discord.app_commands import Parameter
-from commands.types import CommandMapping
+#from discord.app_commands import Parameter
+from commands.types import CommandMapping, Parameter
 import retrieve_players.retrieve_players_main as retrieve_players_main
 
 retrieve_commands: CommandMapping = {
     "retrieve_players": {
         "function": retrieve_players_main.retrieve_player_list,
         "description": "Retrieves list of players of an event",
-        "params": [
-            Parameter(name="startgg_link", descrption="Link of the start.gg event", required=True)
-        ]
+        "params": [Parameter(name="Startgg_link", description="Place a link for a start.gg event", required=True)]
     }
 }
