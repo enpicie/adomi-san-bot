6d26d66b7d54ea3eddd1291e9253bfaa5b665571
Mega <lscomp509@gmail.com>
2025-09-16 14:47:22 -0400

    Added comments to link validation

--- src/retrieve_players/retrieve_action.py
+++ src/retrieve_players/retrieve_action.py
@@ -2,6 +2,10 @@ import re
 from discord import Message
 from retrieve import get_key, get_tourney, get_players, output_list
 
+# Only validates up until the event name
+# Example:
+# Valid: https://www.start.gg/tournament/midweek-melting-27/event/mbaacc-double-elim
+# Invalid: https://www.start.gg/tournament/midweek-melting-27/event/mbaacc-double-elim/overview
 def validate_link(startgg_link: str) -> bool:
     startgg_pattern = re.compile(r"^https:\/\/www.start.gg\/tournament\/([^\/]+)\/event\/([^\/]+)$")
     return re.fullmatch(startgg_pattern, startgg_link)
