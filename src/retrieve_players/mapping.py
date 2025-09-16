191556eb15b7ba8d19fe24e0c1c0b4aa882b1df0
Mega <lscomp509@gmail.com>
2025-09-16 18:22:50 -0400

    Put in no parameters for testing purposes.

--- src/retrieve_players/mapping.py
+++ src/retrieve_players/mapping.py
@@ -1,4 +1,4 @@
-from discord.app_commands import Parameter
+%from discord.app_commands import Parameter
 from commands.types import CommandMapping
 import retrieve_players.retrieve_players_main as retrieve_players_main
 
@@ -6,9 +6,6 @@ retrieve_commands: CommandMapping = {
     "retrieve_players": {
         "function": retrieve_players_main.retrieve_player_list,
         "description": "Retrieves list of players of an event",
-        "params": [Parameter(
-            name="retrieve_player_list", 
-            description="Retrieve a list of players from a start.gg link",
-            required=True)]
+        "params": []
     }
 }
