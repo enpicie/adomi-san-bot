71252cf88f1eedd8808ce10ce216c7ab591a9f82
Mega <lscomp509@gmail.com>
2025-09-16 14:23:03 -0400

    Added retrieve players in the command map

--- src/commands/command_map.py
+++ src/commands/command_map.py
@@ -1,7 +1,8 @@
 from commands.types import CommandMapping
 
 from check_in.mapping import checkin_commands
+from retrieve_players.mapping import retrieve_commands
 
 command_map: CommandMapping = {
     # General commands can be added here
-} | checkin_commands
+} | checkin_commands | retrieve_commands
