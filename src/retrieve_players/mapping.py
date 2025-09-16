1544a37f741011cedacdc3c201395b62f401c50a
Mega <lscomp509@gmail.com>
2025-09-16 18:56:45 -0400

    Added parameter object

--- src/retrieve_players/mapping.py
+++ src/retrieve_players/mapping.py
@@ -1,4 +1,4 @@
-#from discord.app_commands import Parameter
+from discord.app_commands import Parameter
 from commands.types import CommandMapping
 import retrieve_players.retrieve_players_main as retrieve_players_main
 
@@ -6,6 +6,8 @@ retrieve_commands: CommandMapping = {
     "retrieve_players": {
         "function": retrieve_players_main.retrieve_player_list,
         "description": "Retrieves list of players of an event",
-        "params": []
+        "params": [
+            Parameter(name="startgg_link", descrption="Link of the start.gg event", required=True)
+        ]
     }
 }
