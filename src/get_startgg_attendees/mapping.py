bf450665c28ad840a99341b19747b68a1e26b723
Mega <lscomp509@gmail.com>
2025-09-17 19:41:40 -0400

    Added AppCommandOptionType to type Attribute.

--- src/get_startgg_attendees/mapping.py
+++ src/get_startgg_attendees/mapping.py
@@ -1,15 +1,16 @@
+from discord import AppCommandOptionType
 from commands.models.command_mapping import CommandMapping
 from commands.models.command_param import CommandParam
 import get_startgg_attendees.get_startgg_attendees_main as get_startgg_attendees_main
 
 get_attendees_commands: CommandMapping = {
-    "retrieve_players": {
+    "get_startgg_attendees": {
         "function": get_startgg_attendees_main.get_startgg_attendees_list,
         "description": "Retrieves list of players of an event",
         "params": [
             CommandParam(name = "bracket_link",
                          description = "Place a link for a start.gg bracket event",
-                         type = str,
+                         type = AppCommandOptionType.string,
                          required= True
                          )
         ]
