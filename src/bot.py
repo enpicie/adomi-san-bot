26584ec962a8bab527c6bc6cb8b3bbddf51241b4
NOAH LEE <iamnoah@myself.com>
2025-09-17 18:22:58 -0500

    return message via ccustom message model

--- src/bot.py
+++ src/bot.py
@@ -2,16 +2,6 @@ from discord import Message
 
 import constants
 from commands.command_map import command_map
-from commands.models.command_mapping import CommandEntry
-
-def serialize_message(message: Message) -> dict:
-    return {
-        "type": constants.DISCORD_RESPONSE_TYPES["MESSAGE_WITH_SOURCE"],
-        "content": message.content,
-        "embeds": [embed.to_dict() for embed in message.embeds] if message.embeds else [],
-        "attachments": [attachment.to_dict() for attachment in message.attachments] if message.attachments else [],
-        "allowed_mentions": message.allowed_mentions.to_dict() if message.allowed_mentions else {}
-    }
 
 def process_bot_command(event_body: dict) -> dict:
     if "data" not in event_body:
@@ -26,6 +16,6 @@ def process_bot_command(event_body: dict) -> dict:
         raise ValueError(f"No command registered for {command_name}")
     message = command["function"](event_body)
     if message:
-        return serialize_message(message)
+        return message.to_dict()
 
     raise RuntimeError(f"Error processing command '{command_name}': did not return a message.")
--- src/commands/models/command_mapping.py
+++ src/commands/models/command_mapping.py
@@ -1,10 +1,10 @@
 from typing import Callable, List, TypedDict
 
-from discord import Message
+from commands.models.response_message import ResponseMessage
 from commands.models.command_param import CommandParam
 
 class CommandEntry(TypedDict):
-    function: Callable[[dict], Message]
+    function: Callable[[dict], ResponseMessage]
     description: str
     params: List[CommandParam]
 
