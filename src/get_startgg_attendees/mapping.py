47a439a80b838de6cd7634ea8c30f8af33785bc2
Mega <lscomp509@gmail.com>
2025-09-16 20:15:35 -0400

    Made an inline object with properties.

--- src/get_startgg_attendees/mapping.py
+++ src/get_startgg_attendees/mapping.py
@@ -1,17 +1,19 @@
-from discord.app_commands import Parameter
+from types import SimpleNamespace
 from commands.types import CommandMapping
 import get_startgg_attendees.get_startgg_attendees_main as get_startgg_attendees_main
+
+# Source: https://stackoverflow.com/questions/1528932/how-to-create-inline-objects-with-properties
 
 get_attendees_commands: CommandMapping = {
     "retrieve_players": {
         "function": get_startgg_attendees_main.get_startgg_attendees_list,
         "description": "Retrieves list of players of an event",
         "params": [
-            {
-                "name": "bracket_link",
-                "description": "Place a link for a start.gg bracket event",
-                "required": True
-            }
+            SimpleNamespace(
+                name = "bracket_link",
+                description = "Place a link for a start.gg bracket event",
+                required = True
+            )
         ]
     }
 }
