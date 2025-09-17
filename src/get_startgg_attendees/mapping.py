e7b41b8e5c0ede3f227832620ad4f36b60af0a8e
Mega <lscomp509@gmail.com>
2025-09-17 18:43:07 -0400

    Added Command Parameter model

--- src/get_startgg_attendees/mapping.py
+++ src/get_startgg_attendees/mapping.py
@@ -1,19 +1,17 @@
-from commands.types import CommandMapping
+from commands.models.command_mapping import CommandMapping
+from commands.models.command_param import CommandParam
 import get_startgg_attendees.get_startgg_attendees_main as get_startgg_attendees_main
-
-# Source: https://stackoverflow.com/questions/1528932/how-to-create-inline-objects-with-properties
 
 get_attendees_commands: CommandMapping = {
     "retrieve_players": {
         "function": get_startgg_attendees_main.get_startgg_attendees_list,
         "description": "Retrieves list of players of an event",
         "params": [
-            {
-                "name": "startgg_bracket_link",
-                "description": "Place a link for a start.gg bracket event",
-                "type": str,
-                "required": True
-            }
+            CommandParam(name = "bracket_link",
+                         description = "Place a link for a start.gg bracket event",
+                         type = str,
+                         required= True
+                         )
         ]
     }
 }
