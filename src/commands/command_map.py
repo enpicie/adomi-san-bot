4083d6c88a4063749d2feec480e7a8dc17e5ccfc
NOAH LEE <iamnoah@myself.com>
2025-10-11 15:35:24 -0500

    fix check_in reference

--- src/commands/command_map.py
+++ src/commands/command_map.py
@@ -1,6 +1,6 @@
 from commands.models.command_mapping import CommandMapping
 
-from check_in.mapping import checkin_commands
+from commands.check_in.mapping import checkin_commands
 
 command_map: CommandMapping = {
     # General commands can be added here
