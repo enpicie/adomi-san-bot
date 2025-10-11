da43f6b1b75135f09a029ce74f1db4e4ac27ef32
NOAH LEE <iamnoah@myself.com>
2025-10-11 09:51:56 -0500

    fix to use callback types

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,12 @@
 # Discord expects to see this response to its "ping pong" verification request
 PING_PONG_RESPONSE = { "type": 1 }
 # Discord defined message types
-DISCORD_INTERACTION_TYPES =  {
-    "PING": 1,
-    "APPLICATION_COMMAND": 2,
-    "MESSAGE_COMPONENT": 3,
-    "APPLICATION_COMMAND_AUTOCOMPLETE": 4,
-    "MODAL_SUBMIT": 5
+DISCORD_CALLBACK_TYPES =  {
+    "PONG": 1,
+    "MESSAGE_WITH_SOURCE": 4, # CHANNEL_MESSAGE_WITH_SOURCE but omitting CHANNEL for brevity in refs
+    "DEFERRED_MESSAGE_WITH_SOURCE": 5,
+    "DEFERRED_UPDATE_MESSAGE": 6, # only for component interactions
+    "UPDATE_MESSAGE": 7,
+    "APPLICATION_COMMAND_AUTOCOMPLETE_RESULT": 8,
+    "MODAL": 9
 }
