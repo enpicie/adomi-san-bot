040a0b17d339370efa8082bed327e7199212868a
NOAH LEE <iamnoah@myself.com>
2025-10-11 13:04:12 -0500

    set billing to pay per request/on-demand

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,8 +1,9 @@
 resource "aws_dynamodb_table" "adomi_discord_server_table" {
   # Shorten app name for brevity in DynamoDB table name
-  name      = "adomi-discord-server-data-${var.deployment_env}"
-  hash_key  = "PK" # Will be Discord Server ID as "SERVER#<server_id>"
-  range_key = "SK" # Will be sort key as "CONFIG" or "CHANNEL#<channel_id>"
+  name         = "adomi-discord-server-data-${var.deployment_env}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK" # Will be Discord Server ID as "SERVER#<server_id>"
+  range_key    = "SK" # Will be sort key as "CONFIG" or "CHANNEL#<channel_id>"
 
   attribute {
     name = "PK"
