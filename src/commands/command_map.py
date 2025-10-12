a170abb47c3ffd0c68d538b4fd6d6ead5fa0f206
NOAH LEE <iamnoah@myself.com>
2025-10-11 19:55:38 -0500

    remove config commands for now

--- src/commands/command_map.py
+++ src/commands/command_map.py
@@ -1,7 +1,6 @@
 from commands.models.command_mapping import CommandMapping
 
 from commands.check_in.mapping import checkin_commands
-from commands.config.mapping import config_commands
 
 command_map: CommandMapping = {
-} | checkin_commands | config_commands
+} | checkin_commands
