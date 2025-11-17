7b2e79993f88f9aa83ef4b01cde343b40e36c6ee
flea925 <aboodmm@gmail.com>
2025-11-16 17:52:53 -0800

    add delete server command and db logic

--- src/commands/cleanup/commands.py
+++ src/commands/cleanup/commands.py
@@ -5,7 +5,29 @@ import utils.message_helper as msg_helper
 from commands.models.discord_event import DiscordEvent
 from commands.models.response_message import ResponseMessage
 
-def get_all_users(event: DiscordEvent, table: Table) -> ResponseMessage:
+
+def delete_server(event: DiscordEvent, table: Table) -> ResponseMessage:
     """
-    Retrieves all users across all tables 
+    Drops CONFIG server record for the current server
     """
+    server_id = event.get_server_id
+    pk = f"SERVER#{server_id}"
+    sk = "CONFIG"
+    
+    try:
+        with table.batch_writer() as batch:
+            batch.delete_item(
+                Key={"PK": pk, "SK": sk},
+                ConditionExpression="attribute_exists(PK)"  # Ensures the record exists
+                )
+        return ResponseMessage(
+            content=f"Deleted config for server: {server_id}!"
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            return ResponseMessage(
+                content=f"Not found: Server {server_id} does not exist."
+            )
+        # Re-raise unexpected errors
+        raise
+
