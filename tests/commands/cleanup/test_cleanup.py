37876881c30514b7885a653369d2fcbddd4f498e
flea925 <aboodmm@gmail.com>
2025-11-17 21:46:30 -0800

    add test for delete server

--- tests/commands/cleanup/test_cleanup.py
+++ tests/commands/cleanup/test_cleanup.py
@@ -1,11 +1,23 @@
-import pytest
 from unittest.mock import MagicMock
-from botocore.exceptions import ClientError
+from commands.models.discord_event import DiscordEvent
+from commands.models.response_message import ResponseMessage
+import commands.cleanup.commands as cleanup
 
-#TODO implement tests
+def mock_server():
+    return DiscordEvent(
+        {
+            "guild_id": 925,
+            "channel_id": 1
+        }
+    )
 
-# def test_delete_user(monkeypatch):
+def test_delete_server(monkeypatch):
+    mock_table = MagicMock()
+    event = mock_server()
 
-# def test_delete_channel(monkeypatch):
+    response = cleanup.delete_server(event, mock_table)
 
-# def test_delete_server(monkeypatch):
+    # Assertions
+    assert isinstance(response, ResponseMessage)
+    expected_message = f"Deleted config for server: {event.get_server_id()}!"
+    assert response.content == expected_message
