4838b0c979476180650c6da1af49787c6925bbf6
NOAH LEE <iamnoah@myself.com>
2025-11-23 17:23:20 -0600

    add clear-check-ins command

--- src/commands/check_in/mapping.py
+++ src/commands/check_in/mapping.py
@@ -4,7 +4,12 @@ import commands.check_in.check_in_commands as check_in_commands
 checkin_commands: CommandMapping = {
     "check-in": {
         "function": check_in_commands.check_in_user,
-        "description": "Check in the calling user.",
+        "description": "Check in the calling user and assign Participant role if set",
+        "params": []
+    },
+    "clear-check-ins": {
+        "function": check_in_commands.clear_check_ins,
+        "description": "Clear list of checked-in users and remove Participant roles if set",
         "params": []
     }
 }
