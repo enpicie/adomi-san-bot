3a1941d18c258dea1a279f685959e9b35f96bd5c
NOAH LEE <iamnoah@myself.com>
2025-11-23 17:36:16 -0600

    fix param order

--- src/commands/check_in/queue_role_removal.py
+++ src/commands/check_in/queue_role_removal.py
@@ -2,7 +2,7 @@ import json
 from mypy_boto3_sqs.service_resource import Queue
 from typing import List
 
-def enqueue_remove_role_jobs(server_id: str, role_id: str, user_ids: List[str], sqs_queue: Queue):
+def enqueue_remove_role_jobs(server_id: str, user_ids: List[str], role_id: str, sqs_queue: Queue):
     batch = []
 
     for idx, uid in enumerate(user_ids):  # enumerate to get index
