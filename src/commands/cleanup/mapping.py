edbc52420ab53ef13373835108b2bc89a06d5e06
flea925 <aboodmm@gmail.com>
2025-11-24 13:12:07 -0800

    fix very bad typos in package names. append cleanup commands to command_map for registry

--- src/commands/cleanup/mapping.py
+++ src/commands/cleanup/mapping.py
@@ -1,46 +1,8 @@
 from commands.models.command_mapping import CommandMapping
 from discord import AppCommandOptionType
-import commands.check_in.commands as commands
+import commands.cleanup.commands as commands
 
-checkin_commands: CommandMapping = {
-    "get-all-users": {
-        "function": commands.get_all_users,
-        "description": "Retrieve all users in CONFIG table",
-        "params": []
-    },
-    "search-user": {
-        "function": commands.search_user,
-        "description": "Search user given string argument",
-        "params": [
-            {
-                "name": "searchstring",
-                "description": "Search string for DB query",
-                "type": AppCommandOptionType.string
-            }
-        ]
-    },
-    "delete-user": {
-        "function": commands.delete_user,
-        "description": "Delete given user from CONFIG",
-        "params": [
-            {
-                "name": "user",
-                "description": "Discord user passed as argument",
-                "type": AppCommandOptionType.user
-            }
-        ]
-    },
-    "delete-channel": {
-        "function": commands.delete_channel,
-        "description": "Delete given Discord channel",
-        "params": [
-            {
-                "name": "channel",
-                "description": "Discord channel passed as argument",
-                "type": AppCommandOptionType.channel
-            }
-        ]
-    },
+cleanup_commands: CommandMapping = {
     "delete-server": {
         "function": commands.delete_server,
         "description": "Delete server by ID",
