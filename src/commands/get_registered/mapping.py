1bd10d9087bd14ceb79ef7f2ba78e674b9212e7a
NOAH LEE <iamnoah@myself.com>
2025-11-25 19:13:38 -0600

    add show and clear registered comands

--- src/commands/get_registered/mapping.py
+++ src/commands/get_registered/mapping.py
@@ -1,10 +1,21 @@
 from discord import AppCommandOptionType
 
+import commands.get_registered.registered_commands as registered_commands
 import commands.get_registered.startgg.startgg_commands as startgg_commands
 from commands.models.command_mapping import CommandMapping
 from commands.models.command_param import CommandParam
 
 get_registered_commands: CommandMapping = {
+    "show-registered": {
+        "function": registered_commands.show_registered,
+        "description": "Show list of registered users",
+        "params": []
+    },
+    "clear-registered": {
+        "function": registered_commands.clear_registered,
+        "description": "Clear list of registered users",
+        "params": []
+    },
     "get-registered-startgg": {
         "function": startgg_commands.get_registered_startgg,
         "description": "Gets registered participants from a start.gg Tournement Event, replacing existing registered list",
